efc46b3619680892cb77e8abfcf1a3d7046f7b1b
11dev11 <55491319+11dev11@users.noreply.github.com>
2019-09-21 08:54:23 +0200

    Add files via upload

---
+++ for.php
@@ -0,0 +1,14 @@
+<?php
+  for($i=1; $i<=5; $i++){
+    echo "$i <br>";
+  }
+  $luckyNumbers = array(4,8,14,16,23,42);
+  for($i=0; $i<count($luckyNumbers); $i++){
+    echo "$luckyNumbers[$i] <br>";
+  }
+  //commemt
+  /*
+    2 lines
+    comment
+  */
+ ?>
